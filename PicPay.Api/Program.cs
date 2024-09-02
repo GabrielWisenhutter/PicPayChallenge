f9bd03278a2ac6829f192446208b19cf2a4e2207
unknown <gabrielwisenhutter2015@gmail.com>
2024-09-02 09:36:31 -0300

    refactor: construido metodo para resolver as dependencias na api

--- PicPay.Api/Program.cs
+++ PicPay.Api/Program.cs
@@ -3,6 +3,7 @@ using PicPay.Api.Settings;
 using PicPay.Domain.Extensions;
 using PicPay.Domain.Handlers;
 using PicPay.Domain.Interfaces;
+using PicPay.Infra.Data;
 using PicPay.Infra.Data.Context;
 using PicPay.Infra.Data.Repositories;
 using PicPay.Infra.Data.Services;
@@ -10,18 +11,7 @@ using PicPay.Infra.Data.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddDbContext<PicPayContext>(options =>
-{
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
-//builder.Services.AddHandler(typeof(WalletHandler).Assembly);
-builder.Services.AddScoped<IWalletRepository, WalletRepository>();
-builder.Services.AddScoped<ITransferRepository, TransferRepository>();
-builder.Services.AddScoped<ISecurityService, SecurityService>();
-builder.Services.AddScoped<ISecurityService, SecurityService>();
-builder.Services.AddScoped<TransferHandler, TransferHandler>();
-builder.Services.AddScoped<WalletHandler, WalletHandler>();
-builder.Services.AddScoped<Notification>();
+builder.Services.AddPicPay(builder.Configuration);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
---
+++ PicPay.Infra.Data/Container.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PicPay.Domain.Extensions;
+using PicPay.Domain.Handlers;
+using PicPay.Domain.Interfaces;
+using PicPay.Infra.Data.Context;
+using PicPay.Infra.Data.Repositories;
+using PicPay.Infra.Data.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PicPay.Infra.Data
+{
+    public static class Container
+    {
+        public static IServiceCollection AddPicPay(this IServiceCollection services, IConfiguration configuration)
+        {
+            AddDb(services, configuration);
+            AddRepositories(services);
+            AddServices(services);
+            AddHandlers(services);
+
+            return services;
+        }
+
+        private static IServiceCollection AddDb(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<PicPayContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+            });
+
+            return services;
+        }
+
+        private static IServiceCollection AddRepositories(IServiceCollection services)
+        {
+            services.AddScoped<IWalletRepository, WalletRepository>();
+            services.AddScoped<ITransferRepository, TransferRepository>();
+
+            return services;
+        }
+
+        private static IServiceCollection AddServices(IServiceCollection services)
+        {
+            services.AddScoped<ISecurityService, SecurityService>();
+            services.AddScoped<Notification>();
+
+            return services;
+        }
+
+        private static IServiceCollection AddHandlers(IServiceCollection services)
+        {
+            services.AddScoped<WalletHandler, WalletHandler>();
+            services.AddScoped<TransferHandler, TransferHandler>();
+
+            return services;
+        }
+    }
+}
